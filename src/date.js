3b57d72a944688a746ed7f058f0b2d4a7a334006
Mehul Lakhanpal <318097@gmail.com>
2021-06-22 13:22:23 +0530

    Refactor

--- src/date.js
+++ src/date.js
@@ -1,7 +1,10 @@
 import moment from "moment";
 
-const formatDate = (date) => {
-  moment(date).format("DD MMM, YY");
+const formatDate = (date, style = "default") => {
+  if (!date) return null;
+
+  const format = "DD MMM, YY";
+  return moment(date).format(format);
 };
 
 export { formatDate };
