6f2ceebb607dd72c9092085491f1c883a002b98f
Mehul Lakhanpal <318097@gmail.com>
2021-07-20 23:48:09 +0530

    WIP: JS Docs

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,3 +1,8 @@
+/**
+ * Creates/Updates a slug
+ * @param {Object} input - title, seperator & previous slug
+ * @returns {string} - generated slug
+ */
 const generateSlug = ({ title = "", seperator = "-", prevSlug }) => {
   const slug = title
     .trim()
@@ -14,6 +19,10 @@ const generateSlug = ({ title = "", seperator = "-", prevSlug }) => {
   return slug ? `${slug}${seperator}${timestamp}` : "";
 };
 
+/**
+ * Copies a text to clipboard
+ * @param {string} text - text to be copied to clipboard
+ */
 const copyToClipboard = (text) => {
   const textField = document.createElement("textarea");
   textField.innerHTML = text;
@@ -23,11 +32,29 @@ const copyToClipboard = (text) => {
   textField.remove();
 };
 
+/**
+ *
+ * @param {Node} node - DOM node of the scrollable element
+ * @return {boolean} - true if the node is at the bottom
+ */
 const checkScrollAtBottom = (node) => {
   const { scrollHeight, clientHeight, scrollTop } = node;
   return scrollHeight - clientHeight - scrollTop <= 1 && scrollTop > 0;
 };
 
+/**
+ * @typedef {Object} ServerConfiguration
+ * @property {boolean} isProd - connected to prod or dev
+ * @property {('lambda' | 'heroku')} serverType - server source
+ * @property {boolean} returnObject - returns url object if true. default is false
+ * @property {number} port - port for server. default is 7000
+ */
+
+/**
+ * Gets the Server URL for Bubblegum server
+ * @param {ServerConfiguration} config custom configuration object
+ * @returns {string|Object} serverURL or URL object based on the input
+ */
 const getServerURL = ({
   isProd = false,
   serverType = "lambda",
