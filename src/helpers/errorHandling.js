da8a4f01dc4daea537a622064e821914ded5e71f
Mehul Lakhanpal <318097@gmail.com>
2021-12-31 15:10:13 +0530

    Update: Error handling for status code

--- src/helpers/errorHandling.js
+++ src/helpers/errorHandling.js
@@ -1,5 +1,58 @@
-const handleError = (error) => {
-  console.error(error);
+const ERROR_MAPPING = {
+  ACCOUNT_SUSPENDED: {
+    msg: "Account has been temporarily suspended",
+    forceLogout: true,
+  },
+  ACCOUNT_DELETED: { msg: "Account deleted", forceLogout: true },
+  CREDENTIALS_REVOKED: {
+    msg: "Credentials revoked. Login again.",
+    forceLogout: true,
+  },
+  EMAIL_REQUIRED: { msg: "Email is required" }, // submitting email from forgot password
+  USERNAME_AND_PASSWORD_REQUIRED: {
+    // login
+    msg: "Username & Password are required",
+  },
+  USER_NOT_FOUND: { msg: "User not found" }, // login
+  UNAUTHORIZED: { msg: "Unauthorized", forceLogout: true }, // user not found in middle ware
+  INCORRECT_PASSWORD: { msg: "Incorrect password" }, // original password does not match which changing password
+  INVALID_USERNAME_OR_PASSWORD: {
+    msg: "Invalid username/password",
+  }, // login
+  INVALID_EMAIL: { msg: "Invalid email" }, // submitting email from forgot password
+  INVALID_TOKEN: { msg: "Invalid token" },
+  TOKEN_EXPIRED: { msg: "Token expired", forceLogout: true }, // token has been logged out from session
+  INVALID_VERIFICATION_TOKEN: { msg: "Invalid verification token" },
+  NO_TOKEN_FOUND: { msg: "No token found" },
+  EMAIL_OR_USERNAME_REGISTERED: { msg: "Email/Username already exists." },
+};
+
+const handleError = (
+  error,
+  {
+    logout,
+    enableStatusHandling = true,
+    enableForceLogout = true,
+    notify = () => {},
+  } = {}
+) => {
+  // error handling for axios
+  const errorMessage = error.response ? error.response.data : error.message;
+
+  const matchedErrorObj = ERROR_MAPPING[errorMessage];
+
+  if (enableStatusHandling && matchedErrorObj) {
+    notify(matchedErrorObj.msg, "error");
+    if (enableForceLogout && matchedErrorObj.forceLogout) {
+      if (logout) logout();
+      else {
+        setTimeout(() => {
+          localStorage.clear();
+          if (window.location.pathname !== "/login") window.location.reload();
+        }, 2000);
+      }
+    }
+  } else notify(errorMessage, "error");
 };
 
 export { handleError };
