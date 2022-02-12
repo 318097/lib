c94463a8184fc62de1b5b5cdb97b4e3af6bbcc0a
Mehul Lakhanpal <318097@gmail.com>
2022-02-12 17:47:33 +0530

    Fix: Parse products list & append trackingInfo

--- src/downloads/products.js
+++ src/downloads/products.js
@@ -1,25 +1,54 @@
 import { handleError } from "../helpers/errorHandling";
+import { appendQueryParams } from "../utils/mix";
+import _ from "lodash";
+
 const DATA_URL =
   "https://raw.githubusercontent.com/318097/bubblegum/master/PRODUCTS.json";
+
+// Keep this function in sync with 'bubblegum'
+const parseProducts = (products) =>
+  _.map(products, (product) => {
+    const { links } = product;
+    const cta = _.get(links, "landing.url") || _.get(links, "product.url");
+    return {
+      ...product,
+      cta,
+    };
+  });
 
 const getProducts = async (url = DATA_URL) => {
   try {
     const res = await fetch(url);
     const data = await res.json();
     const { products = [] } = data || {};
-    return products;
+    return parseProducts(products);
   } catch (error) {
     handleError(error);
   }
 };
 
-const formatPromotionalProducts = (products = [], appId) => {
+const formatPromotionalProducts = (
+  products = [],
+  { appId, trackingInfo } = {}
+) => {
   if (!appId) return;
-  return {
-    current: products.find((product) => product.id === appId),
-    others: products.filter(
-      (product) => product.id !== appId && product.visibility?.promotion
+  const current = _.find(products, { id: appId });
+  const others = _.map(
+    _.filter(
+      products,
+      ({ id, visibility }) => id !== appId && visibility?.promotion
     ),
+    (product) => {
+      const queryParams = `utm_source=${appId.toLowerCase()}&utm_medium=${
+        trackingInfo?.utm_medium
+      }`;
+      return { ...product, cta: appendQueryParams(product.cta, queryParams) };
+    }
+  );
+
+  return {
+    current,
+    others,
   };
 };
 
